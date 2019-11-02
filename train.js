6a8e431890729474aedbaf02a91aa2c13bde615a
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-01 20:10:24 -0400

    having a really hard time syncing my firebase, and getting my functions to work.

--- train.js
+++ train.js
@@ -1,56 +1,66 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBYITarIGkIw6HFkU42QqWlySlsGR9J3Y8",
-    authDomain: "the-moose-app.firebaseapp.com",
-    databaseURL: "https://the-moose-app.firebaseio.com",
-    projectId: "the-moose-app",
-    storageBucket: "the-moose-app.appspot.com",
-    messagingSenderId: "768484475686",
-    appId: "1:768484475686:web:f095784483a6c4f570f7e7"
-};
-// Firebase initialization
-firebase.initializeApp(firebaseConfig);
+    var firebaseConfig = {
+        apiKey: "AIzaSyBYITarIGkIw6HFkU42QqWlySlsGR9J3Y8",
+        authDomain: "the-moose-app.firebaseapp.com",
+        databaseURL: "https://the-moose-app.firebaseio.com",
+        projectId: "the-moose-app",
+        storageBucket: "the-moose-app.appspot.com",
+        messagingSenderId: "768484475686",
+        appId: "1:768484475686:web:f095784483a6c4f570f7e7"
+    };
+    // Firebase initialization
+    firebase.initializeApp(firebaseConfig);
 
-var database = firebase.database();
+    var database = firebase.database();
 
-$("#submitBtn").on("click", function(event){
-    event.preventDefault();
-});
+    $("#submitBtn").on("click", function (event) {
+        event.preventDefault();
+    });
 
-//train variables and values
-var trainName = $("#acLocal", "#acExpress", "#wilmingtonDe", "#newarkDe", "#trentonNj", "#bfBridge", "#sicNj").val().trim();
-var destination = $("#atlanticCity", "#atlanticCityNj", "#wilmington", "#newark", "#trenton", "#camden", "#SeaIsleCity").val().trim();
+    //train variables and values
+    var trainName = $("#acLocal", "#acExpress", "#wilmingtonDe", "#newarkDe", "#trentonNj", "#bfBridge", "#sicNj").val().trim();
+    var destination = $("#atlanticCity", "#atlanticCityNj", "#wilmington", "#newark", "#trenton", "#camden", "#SeaIsleCity").val().trim();
 
 
-// Assumptions
-var frequency = $("#freeQuincy").val().trim();
+    // Assumptions
+    var frequency = $("#freeQuincy").val().trim();
 
-//4:00 PM
-var firstTime = "16:00";
+    //current time is 7:32 PM (19:32)
+    var acLocal = "";
+    var acExpress = "";
+    var wilmingtonDe = "";
+    var newarkDe = "";
+    var trentonNj = "";
+    var bfBridge = "";
+    var sicNj = "";
+    var destination = "";
+    var firstTrain = "";
+    var frequencyOfTrain = "";
+    
 
-// First Time (pushed back 1 year to make sure it comes before current time)
-var firstTimeConverted = moment(firstTime, "HH:MM").subtract(1, "years");
-console.log(firstTimeConverted);
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:MM").subtract(1, "years");
+    console.log(firstTimeConverted);
 
-// Current Time
-var currentTime = moment();
-console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-// Difference between the times
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-console.log("DIFFERENCE IN TIME: " + diffTime);
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-// Time apart (remainder)
-var tRemainder = diffTime % tFrequency;
-console.log(tRemainder);
+    // Time apart (remainder)
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
 
-// Minute Until Train
-var tMinutesTillTrain = tFrequency - tRemainder;
-console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Minute Until Train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-// Next Train
-var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
